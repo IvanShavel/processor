45489fd8d0b8b62051cbec2f9fe29c4a49a3a046
Andrei <a.dzemianchyk@andersenlab.com>
2019-12-12 17:18:48 +0300

    links2

--- src/main/java/com/stock/processor/controller/RequestController.java
+++ src/main/java/com/stock/processor/controller/RequestController.java
@@ -3,14 +3,17 @@ package com.stock.processor.controller;
 import com.stock.processor.dto.Request;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @RequestMapping(path = "/schedule")
 public interface RequestController {
 
     @ApiOperation(value = "start")
     @PostMapping("/start")
-    public void startSchedule(Request request);
+    public void startSchedule(@Valid @RequestBody Request request);
 
 
 }
--- src/main/java/com/stock/processor/dto/Request.java
+++ src/main/java/com/stock/processor/dto/Request.java
@@ -1,5 +1,7 @@
 package com.stock.processor.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.stock.processor.type.TimeUnit;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,9 +14,13 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Accessors(chain = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Request {
+    @JsonProperty
     private List<String> companyName;
+    @JsonProperty
     private int interval;
+    @JsonProperty
     private TimeUnit timeUnit;
 
 }
